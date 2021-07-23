849b9bb76e3a05a59a989cd21081b56682f6ef4b
timlokers <39838876+timlokers@users.noreply.github.com>
2021-07-23 12:24:42 +0200

    23-07-2021

--- lib/View/keylist_view.dart
+++ lib/View/keylist_view.dart
@@ -3,21 +3,21 @@ import 'package:flutter/material.dart';
 class KeyListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Key-Synergy',
-            style: TextStyle(color: Color.fromARGB(255, 74, 73, 65)),
+        title: 'Welcome to Flutter',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Key-Synergy',
+              style: TextStyle(color: Color.fromARGB(255, 74, 73, 65)),
+            ),
+            backgroundColor: Color.fromARGB(255, 249, 165, 27),
+            centerTitle: true,
           ),
-          backgroundColor: Color.fromARGB(255, 249, 165, 27),
-          centerTitle: true,
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+          body: Text(''),
+    )
+    ,
     );
   }
 }
