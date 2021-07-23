ba38921350f3c6646ba3d102a826dabd91d0320e
timlokers <39838876+timlokers@users.noreply.github.com>
2021-07-23 12:03:18 +0200

    23-07-2021

--- lib/Model/key.dart
+++ lib/Model/key.dart
@@ -1,4 +1,24 @@
-class Key{
+class Key {
+  final int id;
+  final int keyId;
+  final String keyName;
+  final String description;
+  final int userProfileId;
 
+  Key(
+      {required this.id,
+      required this.keyId,
+      required this.keyName,
+      required this.description,
+      required this.userProfileId});
 
+  factory Key.fromJson(Map<String, dynamic> json) {
+    return Key(
+      id: json['id'],
+      keyId: json['keyId'],
+      keyName: json['keyName'],
+      description: json['description'],
+      userProfileId: json['userProfileId'],
+    );
+  }
 }
--- lib/Model/user_pofile.dart
+++ lib/Model/user_pofile.dart
@@ -1,3 +1,24 @@
-class UserProfile{
+class UserProfile {
+  int id;
+  String name;
+  String lastname;
+  String email;
+  String phoneId;
 
+  UserProfile(
+      {required this.id,
+      required this.name,
+      required this.lastname,
+      required this.email,
+      required this.phoneId});
+
+  factory UserProfile.fromJson(Map<String, dynamic> json) {
+    return UserProfile(
+      id: json['id'],
+      name: json['name'],
+      lastname: json['lastname'],
+      email: json['email'],
+      phoneId: json['phoneId'],
+    );
+  }
 }
