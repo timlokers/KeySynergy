4bd43e08a6eb694af2477c143c1ef251fc02a7d8
timlokers <39838876+timlokers@users.noreply.github.com>
2021-07-26 16:56:08 +0200

    26-07-2021

--- lib/View/Widgets/key_list_container.dart
+++ lib/View/Widgets/key_list_container.dart
@@ -1,10 +1,16 @@
+/* Flutter import */
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+/* API import */
 import 'package:key_synergy/API/key_synergy_api.dart';
+
+/* Model import */
 import 'package:key_synergy/Model/user_pofile.dart';
+
 
 class KeyListContainer extends StatelessWidget {
   final KeySynergyAPI _KeySynergyAPI = KeySynergyAPI();
-
 
   @override
   Widget build(BuildContext context) {
@@ -16,18 +22,35 @@ class KeyListContainer extends StatelessWidget {
             return ListView.builder(
                 itemCount: data!.length,
                 itemBuilder: (BuildContext context, int index) {
-                  return Card(
-                    color: Colors.white,
-                    child: ListTile(
-                          title: Text(data[index].name + ' ' + data[index].lastname),
-                    ),
-                  );
+                  return Container(
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.all(5.00),
+                        leading: const Icon(
+                          Icons.account_circle_rounded,
+                          size: 55.0,
+                        ),
+                        title: Container(
+                          padding: const EdgeInsets.all(15.00),
+                          child: Text(
+                              data[index].name + ' ' + data[index].lastname),
+                        ),
+                        subtitle: Container(
+                          alignment: Alignment.topLeft,
+                          padding: const EdgeInsets.fromLTRB(
+                              15.00, 00.00, 0.00, 0.00),
+                          child: const Icon(
+                            Icons.vpn_key,
+                            color: Color.fromARGB(255, 249, 165, 27),
+                          ),
+                        ),
+                      ),
+                      decoration: const BoxDecoration(
+                          border: Border(bottom: BorderSide())));
                 });
           } else if (snapshot.hasError) {
             return Text("${snapshot.error}");
-          }
-          else{
-           return const Text('');
+          } else {
+            return const Text('');
           }
         });
   }
