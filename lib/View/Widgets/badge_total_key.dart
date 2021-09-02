a2c21ca036db6fa4616cc58f1e139bb9907cd1a6
timlokers <39838876+timlokers@users.noreply.github.com>
2021-09-02 15:54:42 +0200

    16-08-2021: V1.03 Basic badge without logic

--- lib/View/Widgets/badge_total_key.dart
+++ lib/View/Widgets/badge_total_key.dart
@@ -10,6 +10,33 @@ class BadgeTotalKey extends StatefulWidget {
 class BadgeTotalKeyState extends State<BadgeTotalKey> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 30.0,
+      width: 30.0,
+      alignment: Alignment.center,
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black45,
+            blurRadius: 15.0, // soften the shadow
+            spreadRadius: 1.0, //extend the shadow
+            offset: Offset(
+              5.0, //move to right
+              3.0, //move to bottom
+            ),
+          ),
+        ],
+      ),
+      child: const Text(
+        '2',
+        style: TextStyle(
+          color: Color.fromARGB(255, 74, 73, 65),
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
   }
-} 
+}
--- lib/View/keylist_view.dart
+++ lib/View/keylist_view.dart
@@ -1,30 +1,33 @@
 import 'package:flutter/material.dart';
 
+import 'Widgets/badge_total_key.dart';
 import 'Widgets/key_list_container.dart';
 
-
 class KeyListView extends StatelessWidget {
-
   String deviceId;
 
   KeyListView(this.deviceId, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              'Key-Synergy',
-              style: TextStyle(color: Color.fromARGB(255, 74, 73, 65)),
-            ),
-            backgroundColor: Color.fromARGB(255, 249, 165, 27),
-            centerTitle: true,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Key-Synergy',
+            style: TextStyle(color: Color.fromARGB(255, 74, 73, 65)),
           ),
-          body: KeyListContainer(deviceId),
-    )
-    ,
+          backgroundColor: Color.fromARGB(255, 249, 165, 27),
+          centerTitle: true,
+          actions: [
+            Container(
+              padding: EdgeInsets.fromLTRB(0.0, 10.0, 20.0, 0.0),
+              child: BadgeTotalKey(),
+            ),
+          ],
+        ),
+        body: KeyListContainer(deviceId),
+      ),
     );
   }
 }
