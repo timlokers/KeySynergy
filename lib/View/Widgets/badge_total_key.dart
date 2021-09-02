858c0dfb943849911edf581ccb3d0b97a5f31a74
timlokers <39838876+timlokers@users.noreply.github.com>
2021-09-02 14:30:38 +0200

    16-08-2021: V1.02 Fixed snackbar constant issue

---
+++ lib/View/Widgets/badge_total_key.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BadgeTotalKey extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return BadgeTotalKeyState();
+  }
+}
+
+class BadgeTotalKeyState extends State<BadgeTotalKey> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
